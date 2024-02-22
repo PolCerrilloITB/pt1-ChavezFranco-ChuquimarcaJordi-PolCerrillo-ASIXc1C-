05728e89f1d439628b659e70f524dbcd5d37e6b1
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-22 12:53:01 +0100

    R1 final

--- R1.py
+++ R1.py
@@ -1,21 +1,23 @@
 import  random
 def desordenar_palabra(palabra):
-        if len(palabra) <= 3:
-            return palabra
-        else:
-            primera = palabra[0]
-            ultima = palabra[-1]
+        if len(palabra) > 2:
+            puntuacion = ''
+            if palabra[-1] in ',.?!;:':
+                puntuacion = palabra[-1]
+                palabra = palabra[:-1]
             intermedio = list(palabra[1:-1])
             random.shuffle(intermedio)
-            desorden = ''.join([primera] + intermedio + [ultima])
+            desorden = palabra[0] + ''.join(intermedio) + palabra[-1] + puntuacion
             return desorden
+        else:
+            return palabra
 def desordenar_texto(frase):
     palabras = frase.split()
     mezcla = [desordenar_palabra(palabra) for palabra in palabras]
     frase_mezcla = ' '.join(mezcla)
     return frase_mezcla
 def main():
-    frase = str(input(" "))
+    frase = str(input(""))
     frase_mezcla = desordenar_texto(frase)
     print("", frase_mezcla)
 main()
