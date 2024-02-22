8ea2bb497226cbbf12cecc3eb2595c8c393f6192
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-22 09:55:22 +0100

    Update

--- R1.py
+++ R1.py
@@ -1,8 +1,21 @@
 import  random
-def pedir_texto(palabra):
-    try:
+def desordenar_palabra(palabra):
         if len(palabra) <= 3:
             return palabra
         else:
-    except:
-        print("La plabra ")
+            primera = palabra[0]
+            ultima = palabra[-1]
+            intermedio = list(palabra[1:-1])
+            random.shuffle(intermedio)
+            desorden = ''.join([primera] + intermedio + [ultima])
+            return desorden
+def desordenar_texto(frase):
+    palabras = frase.split()
+    mezcla = [desordenar_palabra(palabra) for palabra in palabras]
+    frase_mezcla = ' '.join(mezcla)
+    return frase_mezcla
+def main():
+    frase = str(input(" "))
+    frase_mezcla = desordenar_texto(frase)
+    print("", frase_mezcla)
+main()
