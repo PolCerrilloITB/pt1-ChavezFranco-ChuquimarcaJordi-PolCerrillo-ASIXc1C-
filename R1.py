9484c9bcb0d21f9a2e095dac96a4096c0286dd3e
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-02-27 11:25:30 +0100

    Update

--- R1.py
+++ R1.py
@@ -1,3 +1,13 @@
+'''
+Franco Chavez/Jordi Chuquimarca/Pol Cerrillo
+27/02/2023
+ASIXc1C M03 UF2
+Descripción: Partint del problema general cal dividir-lo en problemes més simples, denominats subproblemes.
+Així trobarem les funcions en les quals cal descompondre.
+Un punt important, a tenir en compte en aplicar aquesta descomposició, és que cadascun dels subproblemes
+no es genera arbitràriament, sinó que es planteja com un objectiu parcial, amb entitat pròpia, per resoldre
+el seu problema de nivell superior. Un cop assolits tots aquests objectius parcials, es considera resolt el total.
+'''
 import  random
 def desordenar_palabra(palabra):
         if len(palabra) > 2:
