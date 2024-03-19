68a0b78b69c5cfea01ed84244f992fc81ef3d8dc
franco.chavez.7e7 <franco.chavez.7e7@itb.cat>
2024-03-19 13:56:46 +0100

    19 març franco

--- R2/data_source.py
+++ R2/data_source.py
@@ -8,3 +8,18 @@ Un punt important, a tenir en compte en aplicar aquesta descomposició, és que 
 no es genera arbitràriament, sinó que es planteja com un objectiu parcial, amb entitat pròpia, per resoldre
 el seu problema de nivell superior. Un cop assolits tots aquests objectius parcials, es considera resolt el total.
 '''
+import requests
+API_KEY = 'MjOTkzw75nU7momSX5KDbQ==gVWI97BI0oLZ8eHj'
+frase = "."
+def get_data__from_keyboard():
+    frase = str(input())
+    return frase
+
+def get_data_from_server(frase):
+    frase = input()
+    frase = requests.get(frase, headers={'X-Api-Key': API_KEY})
+    if frase.status_code == 200:
+        return frase.text
+    else:
+        print(f"Error al obtener el input desde la API. Código de estado: {frase.status_code}")
+        return None
