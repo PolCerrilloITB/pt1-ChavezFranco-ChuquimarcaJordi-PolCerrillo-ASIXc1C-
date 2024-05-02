fe069e02ce75b3501d8c2d0f948f9ffcc169b44e
pol.cerrillo.7e7 <pol.cerrillo.7e7@itb.cat>
2024-05-02 09:50:59 +0200

    Update

--- R3/R3.2/data_source.py
+++ R3/R3.2/data_source.py
@@ -13,9 +13,18 @@ import random
 import re
 import os
 import crazy_words
-def get_data_from_file():
-    arxiu_entrada = os.path.join('entrada')
-    with open(arxiu_entrada, 'r') as f:
+
+PATH_ENTRADA = os.path.join('.', 'entrada')
+PATH_SORTIDA = os.path.join('.', 'sortida')
+def llegir_fitxers():
+    arxius = os.listdir(PATH_ENTRADA)
+    for arxiu in arxius:
+        directory = os.path.join(PATH_ENTRADA, arxiu)
+        paraules_desordenades = get_data_from_file(directory, arxiu)
+        processar_paraules(paraules_desordenades)
+        escritura_arxiu_log(arxiu)
+def get_data_from_file(directory, arxiu):
+    with open(directory, 'r') as f:
         paraules = f.read().split()
     paraules_desordenades = set()
     for paraula in paraules:
@@ -23,17 +32,17 @@ def get_data_from_file():
         paraules_desordenades.add(paraula_desordenada)
     return paraules_desordenades
 def processar_paraules(paraules_desordenades):
-    arxiu_sortida = os.path.join('sortida')
+    arxiu_sortida = os.path.join(PATH_SORTIDA, 'paraules_boges.txt')
     with open(arxiu_sortida, 'w') as f:
         f.write(' '.join(paraules_desordenades))
         f.write('\n')
-def escritura_arxiu_log(arxiu_entrada):
-    arxiu_log = os.path.join('logs')
+def escritura_arxiu_log(arxiu):
+    arxiu_log = os.path.join('.', 'logs', 'boges.log')
     try:
         with open(arxiu_log, 'a') as f:
             now = datetime.datetime.now()
-            f.write(f"{now}: S'han processat les paraules de l'arxiu '{arxiu_entrada}'.\n")
+            f.write(f"{now}: S'han processat les paraules de l'arxiu '{arxiu}'.\n")
     except Exception as e:
-        with open('log', 'a') as f:
+        with open(arxiu_log, 'a') as f:
             now = datetime.datetime.now()
             f.write(f"{now}: ERROR - {str(e)}\n")
