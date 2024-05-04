caae99735cf1adbcd868120dfabdd7e046446f60
Pol <pol.cerrillo.7e7@itb.cat>
2024-05-04 11:55:19 +0200

    update a medias

--- R3/R3.2/crazy_words.py
+++ R3/R3.2/crazy_words.py
@@ -13,48 +13,35 @@ caracteres = ['.', ',', '!', '?', ';', ':', '¿', '¡', '(', ')', '"', '=', '€
 
 from crazy_phrase import frase_larga
 import random
-
 def leer_palabras(frase):
     fraseSplit = frase.split()
     frase_final = ""
     for palabra in fraseSplit:
-        if len(palabra) == 1 or len(palabra) == 2 or (len(palabra) == 4 and palabra[-1] in caracteres):
+        if len(palabra) <= 2 or (len(palabra) == 4 and palabra[-1] in caracteres):
             frase_final += palabra + " "
-        elif palabra[0] in numeros or (palabra[0] in caracteres and palabra[2] in numeros) or ("." in palabra and palabra[-1] != "." and palabra[0] != ".") or palabra[1:-1] == palabra[1:-1][::-1]:
+        elif palabra[0] in numeros or (palabra[0] in caracteres and palabra[2] in numeros) or ("." in palabra and palabra[-1]!= "." and palabra[0]!= ".") or palabra[1:-1] == palabra[1:-1][::-1]:
             frase_final += palabra + " "
         elif len(palabra) > 3:
             frase_final = frase_larga(palabra, frase_final)
         else:
             frase_final += palabra + " "
-    return frase_final
+    return frase_final.strip()
 
 def apostrofe_guion(palabra):
-    posicio_caracter_mig = -1
-    tipus_caracter = ""
     if "'" in palabra or "-" in palabra:
         if "'" in palabra:
-            tipus_caracter = "'"
-            posicio_caracter_mig = palabra.index("'")
-            palabra = palabra.replace("'", "")
+            palabra = palabra.replace("'", "").replace("'", "")
         elif "-" in palabra:
-            tipus_caracter = "-"
-            posicio_caracter_mig = palabra.index("-")
-            palabra = palabra.replace("-", "")
-    if posicio_caracter_mig != -1:
-        palabra = list(palabra)
-        if tipus_caracter == "'":
-            palabra.insert(posicio_caracter_mig, "'")
-        else:
-            palabra.insert(posicio_caracter_mig, "-")
-        palabra = ''.join(palabra)
-    return palabra, posicio_caracter_mig, tipus_caracter
+            palabra = palabra.replace("-", "").replace("-", "")
+    if len(palabra) > 1 and palabra[-1] in ["'", "-"]:
+        palabra = palabra[:-1] + palabra[-1] + palabra[-1]
+    return palabra
 
 def mezcla(palabra):
-    palabra, posicio_caracter, tipus_caracter = apostrofe_guion(palabra)
     palabra_inicial = palabra
     if len(palabra) != 3 and (len(palabra) != 4 or palabra[1] != palabra[2]):
         if palabra[1:-1] != palabra[1:-1][::-1]:
             while palabra == palabra_inicial:
                 palabra = ''.join([palabra[0]] + random.sample(palabra[1:-1], len(palabra[1:-1])) + [palabra[-1]])
-    palabra = apostrofe_guion(palabra, posicio_caracter, tipus_caracter)
+    palabra = apostrofe_guion(palabra)
     return palabra
