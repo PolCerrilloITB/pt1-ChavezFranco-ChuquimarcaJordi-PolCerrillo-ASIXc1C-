30b133456beb8f873bc8a83ec86d1b215c0cf4e9
Pol <pol.cerrillo.7e7@itb.cat>
2024-05-04 11:52:19 +0200

    update a medias

--- R3/R3.2/data_source.py
+++ R3/R3.2/data_source.py
@@ -8,41 +8,31 @@ Un punt important, a tenir en compte en aplicar aquesta descomposició, és que 
 no es genera arbitràriament, sinó que es planteja com un objectiu parcial, amb entitat pròpia, per resoldre
 el seu problema de nivell superior. Un cop assolits tots aquests objectius parcials, es considera resolt el total.
 '''
-import datetime
-import random
-import re
 import os
 import crazy_words
+import log_files
+DIR_ENTRADA = os.path.join(".", "entrada")
+DIR_SALIDA = os.path.join(".", "sortida")
 
-PATH_ENTRADA = os.path.join('.', 'entrada')
-PATH_SORTIDA = os.path.join('.', 'sortida')
-def llegir_fitxers():
-    arxius = os.listdir(PATH_ENTRADA)
+def llegir_directori():
+    arxius = os.listdir(DIR_ENTRADA)
     for arxiu in arxius:
-        directory = os.path.join(PATH_ENTRADA, arxiu)
-        paraules_desordenades = get_data_from_file(directory, arxiu)
-        processar_paraules(paraules_desordenades)
-        escritura_arxiu_log(arxiu)
-def get_data_from_file(directory, arxiu):
-    with open(directory, 'r') as f:
-        paraules = f.read().split()
-    paraules_desordenades = set()
-    for paraula in paraules:
-        paraula_desordenada = crazy_words.desordenar_palabra(paraula)
-        paraules_desordenades.add(paraula_desordenada)
-    return paraules_desordenades
-def processar_paraules(paraules_desordenades):
-    arxiu_sortida = os.path.join(PATH_SORTIDA, 'paraules_boges.txt')
-    with open(arxiu_sortida, 'w') as f:
-        f.write(' '.join(paraules_desordenades))
-        f.write('\n')
-def escritura_arxiu_log(arxiu):
-    arxiu_log = os.path.join('.', 'logs', 'boges.log')
-    try:
-        with open(arxiu_log, 'a') as f:
-            now = datetime.datetime.now()
-            f.write(f"{now}: S'han processat les paraules de l'arxiu '{arxiu}'.\n")
-    except Exception as e:
-        with open(arxiu_log, 'a') as f:
-            now = datetime.datetime.now()
-            f.write(f"{now}: ERROR - {str(e)}\n")
+        entrada_dir = os.path.join(DIR_ENTRADA, arxiu)
+        if os.path.isfile(entrada_dir) and entrada_dir.endswith(".txt"):
+            try:
+                with open(entrada_dir, 'r', encoding='utf-8') as f:
+                    linies = f.readlines()
+                f.close()
+                linies_boges = [crazy_words.leer_palabras(linia) + '\n' for linia in linies]
+                escriptura_fitxers(linies_boges, arxiu)
+            except Exception as e:
+                log_files.error_log(e, entrada_dir)
+
+def escriptura_fitxers(frase, arxiu):
+    os.makedirs(DIR_SALIDA, exist_ok=True)
+    output_file_path = os.path.join(DIR_SALIDA, arxiu + "_boges.txt")
+    with open(output_file_path, 'w', encoding='utf-8') as f:
+        f.writelines(frase)
+    f.close()
+
+
