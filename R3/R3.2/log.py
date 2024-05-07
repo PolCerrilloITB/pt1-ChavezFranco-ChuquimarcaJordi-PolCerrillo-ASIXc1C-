88359ed55c82efdd25dc4957f963a9c4b47fcf1d
Pol <145445253+PolCerrilloITB@users.noreply.github.com>
2024-05-07 10:36:41 +0200

    Update log.py

--- R3/R3.2/log.py
+++ R3/R3.2/log.py
@@ -14,12 +14,13 @@ import os
 DIR_LOG = os.path.join(".", "log")
 
 def configurar_logger():
-    os.makedirs(DIR_LOG, exist_ok=True)
+    os.makedirs(DIR_LOG, exist_ok=True) # Crea el directorio de registro si no existe.
     log_programa = "ParaulesBoges"
+    # Formato del registro que incluye la fecha, nombre del logger, nivel de registro y mensaje.
     log_formato = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     log_nivel = logging.DEBUG
     log_modo = 'a'
-    log_archivo = os.path.join(DIR_LOG, 'paraules_boges.log')
+    log_archivo = os.path.join(DIR_LOG, 'paraules_boges.log') # Configura el sistema de registro básico.
     logging.basicConfig(level=log_nivel, format=log_formato, filename=log_archivo, filemode=log_modo)
     return logging.getLogger(log_programa)
 
@@ -33,5 +34,6 @@ def error_log(e, archivo):
 
 def final_log(duracion):
     logger = configurar_logger()
-    duracion_str = "{:.2f}".format(duracion)
+    duracion_str = "{:.2f}".format(duracion) # Formatea la duración a dos decimales.
+    # Registra un mensaje de información sobre la finalización del programa junto con la duración.
     logger.info("Programa finalizado, tardó " + str(duracion_str) + " segundos")
